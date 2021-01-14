79b30e3730cfca5dc32453d8d8e1f5f427c3268c
Kevin Schiffer <kevin.schiffer@thethingsindustries.com>
2021-01-14 18:39:38 +0100

    dev: Rename cypress test oauth occurrences to account app

--- cypress/integration/smoke/authorization/index.js
+++ cypress/integration/smoke/authorization/index.js
@@ -14,35 +14,38 @@
 
 import { defineSmokeTest } from '../utils'
 
-const authorizeConsole = defineSmokeTest('succeeds manually authorizing OAuth clients', () => {
-  const user = {
-    ids: { user_id: 'create-app-test-user' },
-    primary_email_address: 'create-app-test-user@example.com',
-    password: 'ABCDefg123!',
-    password_confirm: 'ABCDefg123!',
-  }
+const authorizeConsole = defineSmokeTest(
+  'succeeds manually authorizing Account App clients',
+  () => {
+    const user = {
+      ids: { user_id: 'create-app-test-user' },
+      primary_email_address: 'create-app-test-user@example.com',
+      password: 'ABCDefg123!',
+      password_confirm: 'ABCDefg123!',
+    }
 
-  cy.createUser(user)
+    cy.createUser(user)
 
-  // Set Console client's `skip_authorization` flag to false.
-  cy.task('execSql', `UPDATE clients SET skip_authorization=false WHERE client_id='console';`)
-  cy.visit(Cypress.config('consoleRootPath'))
+    // Set Console client's `skip_authorization` flag to false.
+    cy.task('execSql', `UPDATE clients SET skip_authorization=false WHERE client_id='console';`)
+    cy.visit(Cypress.config('consoleRootPath'))
 
-  // Login.
-  cy.findByLabelText('User ID').type(user.ids.user_id)
-  cy.findByLabelText('Password').type(`${user.password}{enter}`)
+    // Login.
+    cy.findByLabelText('User ID').type(user.ids.user_id)
+    cy.findByLabelText('Password').type(`${user.password}{enter}`)
 
-  // Authorize.
-  cy.location('pathname').should('contain', `${Cypress.config('oauthRootPath')}/authorize`)
-  cy.findByRole('button', { name: /Authorize Console/ }).click()
+    // Authorize.
+    cy.location('pathname').should('contain', `${Cypress.config('accountAppRootPath')}/authorize`)
+    cy.findByRole('button', { name: /Authorize Console/ }).click()
 
-  // Check Console landing view.
-  cy.location('pathname').should('eq', `${Cypress.config('consoleRootPath')}/`)
-  cy.findByTestId('full-error-view').should('not.exist')
-})
+    // Check Console landing view.
+    cy.location('pathname').should('eq', `${Cypress.config('consoleRootPath')}/`)
+    cy.findByTestId('full-error-view').should('not.exist')
+  },
+)
 
 const abortAuthorization = defineSmokeTest(
-  'succeeds manually aborting OAuth client authorization',
+  'succeeds manually aborting Account App client authorization',
   () => {
     const user = {
       ids: { user_id: 'create-app-test-user' },
@@ -64,7 +67,7 @@ const abortAuthorization = defineSmokeTest(
     cy.findByLabelText('Password').type(`${user.password}{enter}`)
 
     // Deny authorization.
-    cy.location('pathname').should('contain', `${Cypress.config('oauthRootPath')}/authorize`)
+    cy.location('pathname').should('contain', `${Cypress.config('accountAppRootPath')}/authorize`)
     cy.findByRole('button', { name: /Cancel/ }).click()
 
     // Check Console error.
